7edb19439bad76231eb6743fa1abd381d26bcf40
Saumya Gupta <saumyagupta720@gmail.com>
2021-04-09 01:44:01 +0530

    changes

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let weather = {
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`        )
         .then((response) => response.json())
         .then((data) => this.displayWeather(data))
-        .catch((err) => alert('Location not found :)'));
+        .catch((err) => console.log(err),
+        alert('Location not found :)'));
     },
 
     displayWeather : function(data){
